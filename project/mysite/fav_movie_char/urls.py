47d4c8ffbdaac0b4acf016c8df5421652e24c5ec
MaliszewskiKamil <kamcio4112@gmail.com>
2021-01-30 08:37:29 +0100

    added results page. Characters are ordered by number of votes and they have a place.

--- project/mysite/fav_movie_char/urls.py
+++ project/mysite/fav_movie_char/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'charpicker'
+app_name = 'fav_movie_char'
 
 urlpatterns = [
     path('', views.index, name='index'),
--- project/mysite/fav_movie_char/views.py
+++ project/mysite/fav_movie_char/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import request, HttpResponse
-from django.shortcuts import get_object_or_404, get_list_or_404
+from django.http import request, HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, get_list_or_404, reverse
 from .models import Movie, Character
 
 # Create your views here.
@@ -13,19 +13,19 @@ def index(request):
     return render(request, 'fav_movie_char/index.html', context)
 
 def results(request):
-    return render(request, 'vote succeded')
+    characters_list = Character.objects.all().order_by('-votes')
+    context = {
+        'characters_list': characters_list
+    }
+    return render(request, 'fav_movie_char/results.html', context)
 
 def vote(request):
     movies = get_list_or_404(Movie)
     for movie in movies:
         selected_char = movie.character_set.get(pk=request.POST[movie.title])
-        print(selected_char)
-        print(selected_char)
-        print(selected_char)
-        print(selected_char)
         selected_char.votes += 1
         selected_char.save()
-    return HttpResponse('charpicker:results')
+    return HttpResponseRedirect(reverse('fav_movie_char:results'))
 
 
     
