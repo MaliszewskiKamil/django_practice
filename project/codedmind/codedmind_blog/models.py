6873520d01e1fb8e79712ff366be7d7af1145b4c
MaliszewskiKamil <kamcio4112@gmail.com>
2021-02-02 19:36:09 +0100

    Content is now database driven, thumbnails fit the thumbnail div without destroying the aspect ratio

--- project/codedmind/codedmind_blog/models.py
+++ project/codedmind/codedmind_blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+default_article="Hello, this is a default, empty article"
+
+class Article(models.Model):
+    title = models.CharField(max_length=50, primary_key=True)
+    img_path = models.CharField(max_length=150, default='img/default.png')
+    article = models.TextField(default=default_article, null=False)
+
+class Keywords(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    keyword = models.CharField(max_length=100, null=False, default="none")
--- project/codedmind/codedmind_blog/views.py
+++ project/codedmind/codedmind_blog/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import request, HttpResponse
-
+from .models import Article, Keywords
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog/index.html')
+    article_list = Article.objects.all()
+    context = {
+        'article_list': article_list,
+    }
+    
+    return render(request, 'blog/index.html', context)
